d994ab27c87278fa9c3de4f73e57f6b2a9725a3c
Dhyaneshvar <dhyaneshcodespace@gmail.com>
2023-10-23 20:07:54 +0530

    ExernalApi Project Completed

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const loadStoreAPI = () =>{
   fetch("https://fakestoreapi.com/products")
   .then(responce => responce.json())
-  .then(data => console.log(data))
+  //.then(data => console.log(data))
+  .then(data => displayProducts(data))
 } 
+//Get data
+const getProduct=(product)=>{
+  return (`  
+  <div class="product">
+      <img src="${product.image}" >
+      <h2>${product.title}</h2>
+      <h2>${product.category}</h2>
+      <h2>${product.price}</h2>
+    </div>
+   `)
+}
+//Display Data
 
-loadStoreAPI();
+const displayProducts = (products)=>{
+const displayHTML =products.map(product=>getProduct(product))
+const container =document.getElementById("products")
+container.innerHTML = displayHTML.join(" ")
+}
+
+loadStoreAPI();  
